3930765a3ac8168b86a43d7e564964267f73cd39
oopanpan <oopanpan@gmail.com>
2021-03-03 19:28:36 -0800

    commit tree e1644afddf023f7836b3a45019080415782a2edf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+//* THE MAPPY THINGS
+const mapToNegativize = (arr) => arr.map( ele => ele * -1 )
+
+const mapToNoChange = (arr) => arr.map( ele => ele)
+
+const mapToDouble = (arr) => arr.map( ele => ele * 2 )
+
+const mapToSquare = (arr) => arr.map( ele => ele ** 2 )
+
+//* THE REDUCY THINGS
+const reduceToTotal = (arr, startingPoint = 0) => arr.reduce((total, values) => {return total + values}, startingPoint)
+
+const reduceToAllTrue = (arr) => arr.every( ele => !!ele)
+
+const reduceToAnyTrue = (arr) => arr.some( ele => !!ele)
